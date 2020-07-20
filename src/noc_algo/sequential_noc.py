b0a1abb96fa2c20c1d57c51a87e4b7636986ee85
srbhtrvpt <sourabhtiruvaipati@gmail.com>
2020-07-20 15:19:55 +0200

    made changes to run on my env

--- src/noc_algo/sequential_noc.py
+++ src/noc_algo/sequential_noc.py
@@ -18,65 +18,68 @@ def extractSolFromNlpSolver(res):
     u_opt = np.array(u_opt)
     th_opt = np.array(th_opt)
     thdot_opt = np.array(thdot_opt)
-    print(f'u_opt shape: {u_opt.shape}')
-    print(f'th_opt shape: {th_opt.shape}')
-    print(f'thdot_opt shape: {thdot_opt.shape}')
+    print('u_opt shape:', {u_opt.shape})
+    print('th_opt shape:', {th_opt.shape})
+    print('thdot_opt shape:', {thdot_opt.shape})
+    return u_opt, th_opt, thdot_opt
 
 if __name__ == '__main__':
-    #parameters
-    nx = 2;         # state dimension
-    nu = 1;         # control dimension
-    N = 50;         # horizon length
-    x0bar = [np.pi, 0];   # initial state
-    max_speed = 8
-    max_torque=2.
-
-
-    x = MX.sym('x',nx,1);
-    u = MX.sym('u',nu,1);
 
     pend_dyn = PendulumDynamics()
 
-    x_next = pend_dyn.simulate_next_state(x, u);
-    print(f'type x_next: {type(x_next)}')
+    #parameters
+    nx = 2          # state dimension
+    nu = 1          # control dimension
+    N = 50          # horizon length
+    x0bar = [np.pi, 0]    # initial state
+    max_speed = pend_dyn.max_speed
+    max_torque = pend_dyn.max_torque
+
+    x = MX.sym('x',nx,1) 
+    u = MX.sym('u',nu,1) 
+
+
+    x_next = pend_dyn.simulate_next_state(x, u) 
+    print('type x_next:', type(x_next))
     # integrator
-    F = Function('F', [x, u], x_next, \
-                ['x', 'u'], ['x_next']);
-    print(f'F: {F}')
+    df = {}
+    F = Function('F', [x, u], [x_next], \
+                ['x', 'u'], ['x_next'], df) 
+    print('F:', F)
 
     # NLP formulation
     # collect all decision variables in w
-    g = [];
+    g = [] 
     # full state vector w: [u0, theta1, omega1, ....u49, theta50, omega50]
-    w = [];
+    w = [] 
     # constraint on the entire state vector
-    ubw = [];
-    w0 = 0.1;
-    L = 0;
-    X_k = x0bar;
+    ubw = [] 
+    w0 = 0.1 
+    L = 0 
+    X_k = x0bar 
 
     for i in range(N):
         U_name = 'U_' + str(i)
-        U_k = MX.sym(U_name, nu, 1);
-        L = L + X_k[0]**2.0 + 0.1*(X_k[1])**2;
-        L = L + 0.01 * U_k**2;
+        U_k = MX.sym(U_name, nu, 1) 
+        L = L + X_k[0]**2.0 + 0.1*(X_k[1])**2 
+        L = L + 0.01 * U_k**2 
 
-        X_next = F(X_k, U_k);
+        X_next = F(X_k, U_k) 
 
         X_name = 'X_' + str(i+1)
-        X_k = MX.sym(X_name, nx, 1);
+        X_k = MX.sym(X_name, nx, 1) 
 
         ubw = vertcat(ubw, max_torque, inf, max_speed)
         w = vertcat(w, U_k, X_k)
         g = vertcat(g, X_next - X_k)
-    L = L + 10*(X_k[0]**2.0 + X_k[1]**2.0);
+    L = L + 10*(X_k[0]**2.0 + X_k[1]**2.0) 
     # print the dimensions
-    print(f'w shape: {np.shape(w)}, g shape: {np.shape(g)}, ubw shape: {np.shape(ubw)}')
+    print("w shape:" ,np.shape(w), 'g shape:' ,np.shape(g), 'ubw shape:', np.shape(ubw))
     # create nlp solver
     nlp = {"x": w,
            "f": L,
            "g": g}
-    solver = nlpsol('solver','ipopt', nlp);
+    solver = nlpsol('solver','ipopt', nlp) 
 
     arg = {}
     arg["x0"] = w0
@@ -94,3 +97,4 @@ if __name__ == '__main__':
     ax1.plot(th_opt)
     ax1.plot(thdot_opt)
     ax2.plot(u_opt)
+    plt.show()
