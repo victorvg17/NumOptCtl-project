550b61cdf456c1af485fd12ee343f63cf8d67216
victor george <victorvg17@gmail.com>
2020-07-24 07:21:47 +0200

    removed pi in sin term in dynamics. converging now

--- src/noc_algo/simultaneous_gym_constr.m
+++ src/noc_algo/simultaneous_gym_constr.m
@@ -1,24 +1,6 @@
 close all; clear; clc;
 import casadi.*;
 
-function x_next = rk4step(x, u, dynamics, h)
-% one rk4 step
-% inputs:
-%  x             initial state of integration
-%  u             control, kept constant over integration
-%  dynamics      function handle of ode, callable as dynamics(x, u)
-%  h             time step of integration
-% output:
-%  x_next        state after one rk4 step
-
-    k1 = dynamics(x, u);
-    k2 = dynamics(x+h/2.*k1, u);
-    k3 = dynamics(x+h/2.*k2, u);
-    k4 = dynamics(x+h.*k3, u);
-    x_next = x + h/6.*(k1+2*k2+2*k3+k4);
-end
-
-
 %% initial
 
 % parameters
@@ -29,18 +11,20 @@ DT = .1;        % discretization time step
 N_rk4 = 10;     % number of rk4 steps per discretiazion time step
 x0bar = [pi; 0];   % initial state
 
+% build integrator
+% dynamics
 %% variables for the gym pendulum dynamics
-g = 9.8
-m = 1.0
-l = 1.0
+g = 9.8;
+m = 1.0;
+l = 1.0;
 
-t1 = -3.0*self.g/(2*self.l)
-t2 = 3.0/(self.m*self.l**2)
+t1 = -3.0*g/(2*l);
+t2 = 3.0/(m*l^2);
 
 % build integrator
 % dynamics
 % dynamics = @(x,u) [x(2); sin(x(1)) + u];
-dynamics = @(x,u) [x(2); t1*sin(x(1) + pi) + t2*u];
+dynamics = @(x,u) [x(2); t1*sin(x(1)) + t2*u];
 h = DT / N_rk4;             % integration step
 x = MX.sym('x',nx,1);
 u = MX.sym('u',nu,1);
@@ -74,7 +58,7 @@ for i = 1:N
     L = L + 0.01 * sum(U_k.^2);
 
     X_next = F(X_k, U_k);
-
+    
     X_k = MX.sym(['X_',num2str(i+1)], nx, 1);
     ubw = [ubw; inf; 8];
     w = {w{:}, U_k};
